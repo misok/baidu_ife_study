d1431334a0e65b4f9ced3ee776b3166e4a6b22e6
misok <xmisok@gmail.com>
2016-03-20 19:29:47 +0800

    commit task6 and 8

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+'use strict';
+var path = require('path');
+var gulp = require('gulp');
+var less = require('gulp-less');
+
+gulp.task('style', function(){
+    return gulp.src(path.join(__dirname, 'less/*.less'))
+        .pipe(less())
+        .pipe(gulp.dest(path.join(__dirname, 'css')));
+});
+
+gulp.task('default', ['style']);
+gulp.task('watch', ['style']);
